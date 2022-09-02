66adf7769ab889d8d83e74b1b40b1e375b4e14f2
nigexport <rhassan_owolabi@yahoo.com>
2022-09-02 20:24:17 +0100

    updated

--- src/components/LogoSearch/LogoSearch.jsx
+++ src/components/LogoSearch/LogoSearch.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Logo from '../../img/obidattlogo1.png'
+import {UilSearch} from '@iconscout/react-unicons'
 
 const LogoSearch = () => {
   return (
     <div className="LogoSearch">
-        logoSearch
+        <img src={Logo} alt="" />
+        <div className="Search">
+            <input type="text" placeholder='#explore the structure'/>
+            <div className="s-icon">
+                <UilSearch/>
+            </div>
+        </div>
     </div>
   )
 }
