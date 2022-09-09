632972dfc1cc9338feec3d8f55edbe28e938b0df
nigexport <rhassan_owolabi@yahoo.com>
2022-09-09 22:45:26 +0100

    updated

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,86 +1,85 @@
 import React from 'react'
 import './Auth.css'
 import Logo from '../../img/postpic7.png'
-
+import { useState } from 'react';
 
 const Auth = () => {
+  const [isSignUp, setIsSignUp] = useState(true);
+
+  const [data, setData] = useState({firstname: "", lastname: "", password: "", confirmpass: "", username: ""})
+  
+  const [confirmPass, setConfirmPass] = useState(true);
+  const handleChange = (e)=> {
+    setData({...data, [e.target.name]: e.target.value})
+  };
+
+
+  const handleSubmit = (e)=> {
+    e.preventDefault();
+
+    if(isSignUp)
+    {
+      if(data.password !== data.confirmpass) 
+      {
+        setConfirmPass(false)
+      }
+    }
+  };
+
+  const resetform=()=>{
+    setConfirmPass(true);
+    setData({firstname: "", lastname: "", password: "", confirmpass: "", username: "",})
+  };
   return (
-    <div className="Auth">
+    <div className="Auth">      
+      {/* LeftSide */}
         <div className="a-left">
             <img src={Logo} alt="" />
             <div className="Webname">
-              <h1>ObiDatti 2023!</h1>
-              <div>* Peter Obi for President!</div>    
-              <div>* Join this movement for a better Nigeria</div>
-              <div>* The youths must take back their country</div>
-              <div>* 176,846 polling booths, 774 LGAs in 36 states</div>
-              <div>* To win elections, we need 2 volunteers per polling booth</div>
-              <div>* Volunteers will move door to door wooing voters</div>
-              <div>* To win presidency, 150 voters in each polling booth must vote Labor Party</div>
-              <div>* Labor Party must win 25% voters of 24 states in Nigeria</div>
-              <div>* 2019 elections, Buhari - 15.2 million votes, Atiku - 11.3 million. GO AND VERIFY!</div>
-                                              
+              <h1>ObiDatti 2023! Let's take back our country. We need 20 million votes to win this election.</h1>                                             
             </div>
         </div>
-        <LogIn />
+
+        {/* RightSide */}
+        <div className="a-right">
+          <form action="" className="infoForm authForm" onSubmit={handleSubmit}>
+            <h3>{isSignUp ? "Sign up. To Join The Structure":"Log In. To Continue The Movement "}</h3>                     
+
+            {isSignUp && (
+              <div>
+              <input type="text" placeholder='First Name' className='infoInput' name='firstname' onChange={handleChange}/>
+              <input type="text" placeholder='Last Name' className='infoInput' name='lastname' onChange={handleChange}/>
+            </div>
+
+            )}        
+        
+        <div>
+          <input type="text" className="infoInput" name='username' placeholder='Username' onChange={handleChange}/>
+        </div>
+
+        <div>
+          <input type="password" className="infoInput" name='password' placeholder='Password' onChange={handleChange} />
+          
+          {isSignUp && (
+            <input type="password" className="infoInput" name='confirmpass' placeholder='Confirm Password' onChange={handleChange} value={data.confirmpass}/>
+          )}
+          
+        </div>
+        <span style={{display: confirmPass? "none": "block", color: 'red', fontSize: '12px', alignSelf: "flex-end", marginRight: "5px"}}>
+          * Confirm Password is not same
+        </span>
+        <div>
+          <span style={{fontSize: "12px", cursor: "pointer"}} onClick = {() => {setIsSignUp((prev) => !prev); resetform()}}>
+            {isSignUp ? "Already have an account. Login!": "Don't have an account? Sign Up"}
+            </span>
+        </div>
+        <button className="button infoButton" type='submit'>
+          {isSignUp ? "Sign up": "Log In"}
+          </button>
+      </form>
+    </div>
     </div>
   );
 };
-function LogIn() {
-  return (
-    <div className="a-right">
-      <form className='infoForm authForm'>
-        <h3>Log In</h3>
-
-        <div>
-          <input type="text" placeholder="Username" className="infoInput" name="username" />
-        </div>
-
-        <div>
-        <input type="password" placeholder="Password" className="infoInput" name="password" />
-        </div>
-
-        <div>
-          <span style={{ fontSize: "12px" }}>
-            I don't have an account. Sign up!
-          </span>
-          <button className='button infoButton'>Login</button>
-          </div>        
-      </form>
-    </div>
-  );
-};
-
-
-function SignUp() {
-  return(
-    <div className="a-right">
-      <form action="" className="infoForm authForm">
-        <h3>Sign up</h3>
-
-        <div>
-          <input type="text" placeholder='First Name' className='infoInput' name='firstname' />
-          <input type="text" placeholder='Last Name' className='infoInput' name='lastname' />
-
-        </div>
-
-        <div>
-          <input type="text" className="infoInput" name='username' placeholder='Username' />
-        </div>
-
-        <div>
-          <input type="text" className="infoInput" name='password' placeholder='Password' />
-          <input type="text" className="infoInput" name='confirmpass' placeholder='Confirm Password' />
-
-        </div>
-
-        <div>
-          <span style={{fontSize: '12px'}}>I already have an account. Login!</span>
-        </div>
-        <button className="button infoButton" type='submit'>Signup</button>
-      </form>
-    </div>
-  )
-}
 
 export default Auth
